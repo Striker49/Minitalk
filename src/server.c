92d5109b239b47e5b1f07acda57caed495a70cac
Sébastien Roy <119885387+Striker49@users.noreply.github.com>
2023-07-12 18:09:11 -0400

    Add files via upload
    Server can receive a signal and do something with it!

--- src/server.c
+++ src/server.c
@@ -6,20 +6,57 @@
 /*   By: seroy <seroy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 14:31:23 by seroy             #+#    #+#             */
-/*   Updated: 2023/07/06 11:29:29 by seroy            ###   ########.fr       */
+/*   Updated: 2023/07/12 18:03:43 by seroy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void	sig_handler(int sig, siginfo_t *info, void *ucontext)
+{
+	static int 	base;
+	static int 	i;
+	int 		j;
+	char 		c;
+	static char bin;
+	
+	(void)*ucontext;
+	if (sig == SIGUSR1)
+		printf("good\n");
+
+	// i = 7;
+	// j = 0;
+	// base = 1;
+	// bin = 0;
+	// while (i >= 0)
+	// {
+	// 	if (sig == SIGUSR1) 
+	// 		bin += base;
+	// 	base = base * 2;
+	// 	i--;
+	// }
+	// printf("%d", bin);
+	// i = 7;
+	// j++;
+	// return ;
+}
+
 int	main(void)
 {
-	pid_t pid;
+	struct 		sigaction sa;
+	pid_t 		pid;
+	siginfo_t 	siginfo;
 	
 	pid = getpid();
 	printf("Server PID:%d\n", pid);
+	
+	sigemptyset (&sa.sa_mask);
+	sa.sa_flags = SIGINFO;
+	sa.sa_sigaction = &sig_handler;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 
 	while (1)
-		pause();
+		sleep(1);
 	return (0);
 }
--- src2/client.c
+++ src2/client.c
@@ -6,50 +6,56 @@
 /*   By: seroy <seroy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/27 14:31:18 by seroy             #+#    #+#             */
-/*   Updated: 2023/07/06 16:50:42 by seroy            ###   ########.fr       */
+/*   Updated: 2023/07/12 18:07:39 by seroy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int	sig_handler(char *str)
+void	send_message(char *str, pid_t pid, siginfo_t *info)
 {
-	int i;
-	int j;
-	char c;
-	int bin;
+	static int i;
+	static int j;
+	static char c;
 
-	i = 7;
-	j = 0;
-	while (str[j])
-	{
-		c = str[j];
-		while (i >= 0)
-		{
-			bin = (c >> i) & 1;
-			printf("%d", bin);
-			i--;
-		}
-		printf("\n");
-		i = 7;
-		j++;
-	}
-	return (bin);
+	kill(pid, SIGUSR1);
+	usleep(100);
+
+	// j = 0;
+	// while (str[j])
+	// {
+	// 	i = 7;
+	// 	c = str[j];
+	// 	while (i >= 0)
+	// 	{
+	// 		if ((c >> i) & 1)
+	// 		{
+	// 			kill(pid, SIGUSR1);
+	// 			usleep(100);
+	// 		}
+	// 		else
+	// 		{
+	// 			kill(pid, SIGUSR2);
+	// 			usleep(100);				
+	// 		}
+	// 		i--;
+	// 	}
+	// 	j++;
+	// }
+	return ;
 }
 
 int main(int argc, char **argv)
 {
-	struct sigaction sa;
-	int	pid;
-	int i;
-	
-	// sa.sa_sigaction = sig_handler;
-	// sigemptyset (SIGINFO);
-	sa.sa_flags = 0;
+	struct sigaction	sa;
+	siginfo_t			info;
+	int					pid;
+
 	if (argc < 2)
 		printf("ERROR\nMissing arguments");
 	if (argc > 3)
 		printf("ERROR\nToo many arguments");
-	pid = sig_handler(argv[2]);
+	pid = ft_atoi(argv[1]);
+	send_message(argv[2], pid, &info);
     return (0);
 }
